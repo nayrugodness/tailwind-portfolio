1cb6e9ab1288148df666164496bac280c95f1abf
Nayru <nanualexandraramirez@gmail.com>
2022-05-09 19:36:14 -0500

    adding section

--- src/layouts/Index.jsx
+++ src/layouts/Index.jsx
@@ -1,12 +1,11 @@
-
-
+import Header from "../components/Header.jsx";
+import Section from "../components/Section.jsx";
 const Index = () => {
     return(
         <>
-            <section className="flex flex-row">
-                <h1>Nayru</h1>
-                <h2>Otro</h2>
-                <h3>otro</h3>
+            <section className="flex flex-column flex-wrap">
+                <Header></Header>
+                <Section></Section>
             </section>
         </>
     )
